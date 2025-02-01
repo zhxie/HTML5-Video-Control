e14d51b664a28deba982a11d03ca2af9ef3332c7
Xie Zhihao <xzh1206@gmail.com>
2025-02-01 15:25:46 +0800

    Format

--- HTML5 Video Control.user.js
+++ HTML5 Video Control.user.js
@@ -9,114 +9,121 @@
 // @grant       none
 // ==/UserScript==
 
+const DefaultFastForwardRate = 3.0;
+const DefaultFastForwardStep = 0.1;
+
 (function () {
-    var $tip = document.createElement('p');
-    $tip.id = 'h5-video-control-tip';
-    $tip.style.backgroundColor = '#0000007f';
-    $tip.style.boxSizing = 'border-box';
-    $tip.style.borderRadius = '4px';
-    $tip.style.color = 'white';
-    $tip.style.fontSize = '28px';
-    $tip.style.padding = '2px 8px 2px 8px';
-    $tip.style.position = 'fixed';
-    $tip.style.textShadow =
-        '1px 1px black, -1px 1px black, 1px -1px black, -1px -1px black';
-    $tip.style.verticalAlign = 'center';
-    $tip.style.zIndex = '9999999999';
+    const tip = document.createElement("p");
+    tip.id = "h5-video-control-tip";
+    tip.style.backgroundColor = "#0000007f";
+    tip.style.boxSizing = "border-box";
+    tip.style.borderRadius = "4px";
+    tip.style.color = "white";
+    tip.style.fontSize = "28px";
+    tip.style.padding = "2px 8px 2px 8px";
+    tip.style.position = "fixed";
+    tip.style.textShadow = "1px 1px black, -1px 1px black, 1px -1px black, -1px -1px black";
+    tip.style.verticalAlign = "center";
+    tip.style.zIndex = "9999999999";
 
-    var defaultFastForwardRate = 3.0;
-    var defaultFastForwardStep = 0.1;
+    let scriptEnabled = true;
 
-    var scriptEnabled = true;
-    var toggleTimeout;
-
-    var toggleTipView = function () {
-        document.body.appendChild($tip);
+    let toggleTimeout;
+    const toggleTipView = () => {
+        document.body.appendChild(tip);
         if (toggleTimeout) {
             clearTimeout(toggleTimeout);
             toggleTimeout = null;
-            $tip.innerText = '';
+            tip.innerText = "";
         }
         toggleTimeout = setTimeout(function () {
-            $tip.innerText = '';
+            tip.innerText = "";
             toggleTimeout = null;
-            document.body.removeChild($tip);
+            document.body.removeChild(tip);
         }, 1000);
     };
 
-    var initTipViewPosition = function ($ref) {
-        var offset = $ref.getBoundingClientRect();
-        $tip.style.top = offset.top + 15 + 'px';
-        $tip.style.left = offset.left + 15 + 'px';
+    const initTipViewPosition = (ele) => {
+        const offset = ele.getBoundingClientRect();
+        tip.style.top = offset.top + 15 + "px";
+        tip.style.left = offset.left + 15 + "px";
     };
 
-    addEventListener('keydown', function (ev) {
-        var $ele = document.querySelector('VIDEO');
-        if ($ele) {
-            var activeElement = document.activeElement;
-            var tagName = activeElement.tagName;
-            if (tagName === 'INPUT' || tagName === 'TEXTAREA') return;
+    addEventListener("keydown", (ev) => {
+        const ele = document.querySelector("VIDEO");
+        if (ele) {
+            // Block on inputs.
+            const tagName = document.activeElement.tagName;
+            if (tagName === "INPUT" || tagName === "TEXTAREA") {
+                return;
+            }
 
-            // Block on combination key
-            if (ev.altKey || ev.shiftKey || ev.ctrlKey) {
-                // releaseFastForward($ele);
-                // ev.preventDefault();
+            // Block on combination keys.
+            if (ev.altKey || ev.shiftKey || ev.ctrlKey || ev.metaKey) {
                 return;
             }
 
             toggleTipView();
-            initTipViewPosition($ele);
+            initTipViewPosition(ele);
             switch (ev.key) {
-                case 'q':
+                case "q":
                     if (scriptEnabled) {
                         scriptEnabled = false;
-                        $tip.innerText = 'HTML5 Video Control Disabled';
+                        tip.innerText = "HTML5 Video Control Disabled";
                     } else {
                         scriptEnabled = true;
-                        $tip.innerText = 'HTML5 Video Control Enabled';
+                        tip.innerText = "HTML5 Video Control Enabled";
                     }
                     break;
-                case 'x':
-                    if (!scriptEnabled) return;
-                    if ($ele.playbackRate > defaultFastForwardStep) {
-                        $ele.playbackRate = (
-                            $ele.playbackRate - defaultFastForwardStep
-                        ).toFixed(1);
+                case "x":
+                    if (!scriptEnabled) {
+                        return;
                     }
-                    $tip.innerText = 'Speed: ' + $ele.playbackRate.toFixed(1);
+                    if (ele.playbackRate > DefaultFastForwardStep) {
+                        ele.playbackRate = (ele.playbackRate - DefaultFastForwardStep).toFixed(1);
+                    }
+                    tip.innerText = `Speed: ${ele.playbackRate.toFixed(1)}`;
                     break;
-                case 'c':
-                    if (!scriptEnabled) return;
-                    $ele.playbackRate = (
-                        $ele.playbackRate + defaultFastForwardStep
-                    ).toFixed(1);
-                    $tip.innerText = 'Speed: ' + $ele.playbackRate.toFixed(1);
+                case "c":
+                    if (!scriptEnabled) {
+                        return;
+                    }
+                    ele.playbackRate = (ele.playbackRate + DefaultFastForwardStep).toFixed(1);
+                    tip.innerText = `Speed: ${ele.playbackRate.toFixed(1)}`;
                     break;
-                case 'z':
-                    if (!scriptEnabled) return;
-                    if ($ele.playbackRate === 1) {
-                        $ele.playbackRate = defaultFastForwardRate;
+                case "z":
+                    if (!scriptEnabled) {
+                        return;
+                    }
+                    if (ele.playbackRate === 1) {
+                        ele.playbackRate = DefaultFastForwardRate;
                     } else {
-                        $ele.playbackRate = 1;
+                        ele.playbackRate = 1;
                     }
-                    $tip.innerText = 'Speed: ' + $ele.playbackRate.toFixed(1);
+                    tip.innerText = `Speed: ${ele.playbackRate.toFixed(1)}`;
                     break;
-                case 'd':
-                    if (!scriptEnabled) return;
-                    !$ele.paused && $ele.pause();
-                    if ($ele.currentTime > 1) {
-                        $ele.currentTime -= Number(1 / 30);
+                case "d":
+                    if (!scriptEnabled) {
+                        return;
                     }
-                    $tip.innerText =
-                        'Frame: ' + (30 * $ele.currentTime).toFixed(0);
+                    if (!ele.paused) {
+                        ele.pause();
+                    }
+                    if (ele.currentTime > 1) {
+                        ele.currentTime -= Number(1 / 30);
+                    }
+                    tip.innerText = "Frame: " + (30 * ele.currentTime).toFixed(0);
                     break;
-                case 'f':
-                case 'g':
-                    if (!scriptEnabled) return;
-                    !$ele.paused && $ele.pause();
-                    $ele.currentTime += Number(1 / 30);
-                    $tip.innerText =
-                        'Frame: ' + (30 * $ele.currentTime).toFixed(0);
+                case "f":
+                case "g":
+                    if (!scriptEnabled) {
+                        return;
+                    }
+                    if (!ele.paused) {
+                        ele.pause();
+                    }
+                    ele.currentTime += Number(1 / 30);
+                    tip.innerText = "Frame: " + (30 * ele.currentTime).toFixed(0);
                     break;
                 default:
                     break;
